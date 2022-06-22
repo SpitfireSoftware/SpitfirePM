c9ae0cb62eaea10139458d9eacdba99c36fd270e
Stan York <development@SpitfireManagement.com>
2022-06-21 22:51:56 -0400

    1.30.172 Add REST GetChangedActionItems

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -12,7 +12,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { getDriver } from "localforage";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "1.30.171";
+const ClientPackageVersion : string = "1.30.172";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou
 
