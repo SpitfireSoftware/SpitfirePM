7df660f8225fa74775944032942876c2b2c3950f
Stan York <syork@spitfiremanagement.com>
2023-05-21 22:51:01 -0400

    23.8539.3 adds priority  to FileInfo

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -11,7 +11,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8539.2";
+const ClientPackageVersion : string = "23.8539.3";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou
 
