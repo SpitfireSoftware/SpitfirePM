52e799aeb30ead5e25fe8ebdcd05699abc311cd0
Stan York <syork@spitfiremanagement.com>
2023-08-03 21:56:11 -0400

    23.8615.1 get recently deleted doc list

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -11,7 +11,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8611.1";
+const ClientPackageVersion : string = "23.8615.2";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou
 
