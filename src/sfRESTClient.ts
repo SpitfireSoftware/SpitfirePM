2ef689e7b305665dea26f6b9378075c11088811a
Stan York <syork@spitfiremanagement.com>
2023-08-24 15:35:18 -0400

    23.8637.1  PagePK handles diagTools

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -11,7 +11,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8615.3";
+const ClientPackageVersion : string = "23.8637.1";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou
 
@@ -3162,7 +3162,7 @@ public CreateButtonElement(withClass: undefined | string, withTip:string|undefin
             result = sfRestClient._WCC.Project;
             else {
                 const pageName = this.ResolvePageName();
-                if (["executiveDashboard","catalog","home","pivot"].find(el=>el=== pageName) ) noResultOK = true;
+                if (["executiveDashboard","catalog","home","pivot","diagnostic-tools"].find(el=>el=== pageName) ) noResultOK = true;
             }
         if (!result) console.warn(`GetPagePK could not resolve key for [${this.ResolvePageName()}]`);
         return result;
