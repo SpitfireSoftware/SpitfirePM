e6cad7de55ad1caaf558d00fd76d2878d46c2173
Stan York <syork@spitfiremanagement.com>
2023-12-29 17:13:49 -0500

    23.8764.0  Google Auth

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -11,7 +11,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8750.4";
+const ClientPackageVersion : string = "23.8764.0";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou
 
