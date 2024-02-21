ede35535e18d81867fc50d61555f55f35217d678
Stan York <syork@spitfiremanagement.com>
2024-02-21 17:59:19 -0500

    23.8810.1  Flush single session

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -10,7 +10,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8801.2";
+const ClientPackageVersion : string = "23.8810.1";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou of XB Software
 
