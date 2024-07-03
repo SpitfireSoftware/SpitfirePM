e2bdcd856428d4e6b7d4f0cb7bdf87caec93324f
Stan York <syork@spitfiremanagement.com>
2024-07-02 21:14:53 -0400

    23.8940.1 Support 404 from patchRoute

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -10,7 +10,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.8910.6";
+const ClientPackageVersion : string = "23.8940.1";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou of XB Software
 
