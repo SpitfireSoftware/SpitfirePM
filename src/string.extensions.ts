0dd234b23924516864e6582b85ecc48bac890850
Stan York <syork@spitfiremanagement.com>
2025-07-14 11:18:38 -0400

    23.9300.8 Fix bug in classic postback handler (__doPostBack)

--- src/string.extensions.ts
+++ src/string.extensions.ts
@@ -103,19 +103,19 @@ if (!Window.prototype.__sfPostBackTargetForm) {
     Window.prototype.__sfPostBackTargetForm = function():HTMLFormElement | undefined {
         var DefaultFormName: string = "Form1";
         var theForm : HTMLFormElement | undefined;
-        if (DefaultFormName in this.document.forms) theForm = document.forms[<any>DefaultFormName];
+        if (DefaultFormName in self.document.forms) theForm = document.forms[<any>DefaultFormName];
         return theForm;
     }
 }
 if (!Window.prototype.__hasPostBackTarget) {
     Window.prototype.__hasPostBackTarget = function(): boolean {
-        var theForm : HTMLFormElement | undefined = this.__sfPostBackTargetForm();
+        var theForm : HTMLFormElement | undefined = self.__sfPostBackTargetForm();
         return theForm !== undefined;
     }
 }
 if (!Window.prototype.__doPostBack) {
     Window.prototype.__doPostBack = function(eventTarget, eventArgument) {
-        var theForm : HTMLFormElement | undefined = this.__sfPostBackTargetForm();
+        var theForm : HTMLFormElement | undefined = self.__sfPostBackTargetForm();
         if (!theForm) {
             console.warn("No form found for classic postbacks");
         }
