4a4a646ae95adedca8a62c0b587ad2d17968ef55
Stan York <syork@spitfiremanagement.com>
2025-09-16 18:50:57 -0400

    23.9330.9 AddDocRoute response codes

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -8,7 +8,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.9330.8";
+const ClientPackageVersion : string = "23.9330.9";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou of XB Software
 
