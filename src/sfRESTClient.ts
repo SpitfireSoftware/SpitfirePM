13d643e45982876d27fa622d1239f6e456e1b348
Stan York <syork@spitfiremanagement.com>
2025-12-30 11:09:56 -0500

    23.9400.12 Additional fields for ProjectDocsOfType

--- src/sfRESTClient.ts
+++ src/sfRESTClient.ts
@@ -8,7 +8,7 @@ import  * as RESTClientBase from "./APIClientBase"; // avoid conflict with same 
 import { sfApplicationRootPath, sfProcessDTKMap } from "./string.extensions";
 //import {dialog}    from "jquery-ui";
 
-const ClientPackageVersion : string = "23.9400.11";
+const ClientPackageVersion : string = "23.9400.12";
 
 // originally modified for typescript and linter requirements by Uladzislau Kumakou of XB Software
 
